3dd61ac8b831a9d69d4c579fbec7007b81a50f40
ntkzo <ntokozo.skosana@gmail.com>
2023-02-21 00:43:14 -0800

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main_h"
+
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'i'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
