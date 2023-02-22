c432f7e75978409e367c0051f474bafb968dcd30
ntkzo <ntokozo.skosana@gmail.com>
2023-02-22 06:26:42 -0800

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - prints _putchar and a new line
  * Return: Always 0
  */
-
 int main(void)
-
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
 
+	return (0);
 }
