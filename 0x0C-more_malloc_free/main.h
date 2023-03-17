f844a101d11ea6cec7b107d8a4a0c9f0c0f5a080
ntkzo <ntokozo.skosana@gmail.com>
2023-03-17 11:53:00 -0700

    task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,5 +13,8 @@
 	void *_calloc(unsigned int nmemb, unsigned int size);
 	int *array_range(int min, int max);
 	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+	void errors(void);
+	int _strlen(char *s);
+	int is_digit(char *s);
 
 	#endif
